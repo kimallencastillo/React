d11093badd969d67f8762498d3d317f41fe9522e
kimallencastillo <kimallencastillo@gmail.com>
2023-01-28 17:14:40 +0800

    adding css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 
+import React,{ useState } from 'react';
 import './App.css';
+import Employee from './components/employee';
 
 function App() {
+  console.log("We Are About to list the Employees");
+  const showEmployees = true;
+  //let role = 'dev';
+  
+  // using useState
+  const [role, setRole] = useState('dev');
+  // No. 1 of using state never assign a value to the variable directly you always go through set whatever the set is in this case set role
   return (
     <div className="App">
-      <header className="App-header">
+      {showEmployees ?
+      <>
+      <input type='text' onChange={(e)=> {
+        console.log(e.target.value);
+        setRole(e.target.value)
+      }} />
+        <Employee name="Allen" role="intern"/>
+        <Employee name="John" role={role}/>
+        <Employee name="Clark" />
+      
+      </>
+        : 
+      <p>You cannot see the employees</p>
+      }
 
-        <p>
-          SAMPLE
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
---
+++ src/components/employee.js
@@ -0,0 +1,12 @@
+function Employee(props) {
+    
+    return (
+        <>
+        <h3>Employee {props.name} </h3>
+        <p>{props.role ? props.role : "No role"}</p>  
+        {/*props.role ? <p className="role">{props.role}</p> : <p className="norole">No role</p>*/}
+        </>
+    )
+}
+
+export default Employee;
